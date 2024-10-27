c1ea4ba7d20823bbed23219e1a09fce64803db2f
Amrita965 <amrita15-3792@diu.edu.bd>
2024-10-27 16:22:28 +0600

    added course class

---
+++ course/course.py
@@ -0,0 +1,14 @@
+
+class Course:
+    
+    def __init__(self, course_name, course_code, insturctor):
+        self.course_name = course_name
+        self.course_code = course_code
+        self.insturctor = insturctor
+        
+    def add_student(self, student):
+        pass
+    
+    def display_course_info(self):
+        pass
+        
