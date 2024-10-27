d315302983951b27ad1d23e715a2fed1f2132fdc
Amrita965 <amrita15-3792@diu.edu.bd>
2024-10-27 16:41:12 +0600

    added StudentManagementSystem class

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 
+class StudentManagementSystem:
+    
+    def add_student(self, name, age, address, student_id):
+        pass
+    
+    def add_course(self, course_name, course_code, instructor):
+        pass
+    
+    def enroll_in_course(self, studentId, course_code):
+        pass
+    
+    def add_grade(self, studentId, course_code):
+        pass
+    
+    def display_student_details(self):
+        pass
+    
+    def display_course_details(self):
+        pass
+    
+        
+    
